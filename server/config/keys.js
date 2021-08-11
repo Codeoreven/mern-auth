ca994c1d47390378072cd65fc796c088e816f6ed
unknown <codeoreven@gmail.com>
2021-08-11 07:39:02 +0100

    created server, added hashing of the password, login and register options are working

---
+++ server/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI = "mongodb+srv://User1:User1$@cluster0.9szj3.mongodb.net/Cluster0?retryWrites=true&w=majority"
+}
---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+
+dotenv.config();
+
+const app = express();
+
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+}).then(() => console.log("DB connection successfulll")).catch((err) => console.log(err));
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+
+app.listen(8800, () => {
+    console.log("Backend server is running");
+});
